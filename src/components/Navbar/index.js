ce7e4bc2904cb7cbf0e4fac0829f3f4e99b65e24
Akash Kumar <akaysharma894@gmail.com>
2021-06-21 20:43:33 +0530

    Added navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Styles from "./navbar.module.css";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { ReactComponent as Logo2 } from "../../assets/logo2.svg";
+import { ReactComponent as Bell } from "../../assets/icons/bell.svg";
+import { ReactComponent as Help } from "../../assets/icons/help.svg";
+
+const Navbar = () => {
+  return (
+    <nav className={Styles["navbar-wrapper"]}>
+      <div className={Styles["nav-logo"]}>
+        <Logo className={Styles["logo"]} />
+        <Logo2 className={Styles["logo-text"]} />
+      </div>
+      <div className={Styles["nav-items"]}>
+        <div className={Styles["current-user"]}>
+          <div className={Styles["profile"]}></div>
+          <p>Kishore</p>
+        </div>
+        <div className={Styles["nav-icons"]}>
+          <span className={Styles["nav-item-icon"]}>
+            <Bell className={Styles["bell"]} />
+          </span>
+          <span className={Styles["nav-item-icon"]}>
+            <Help className={Styles["help"]} />
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
