e02d83160d2d75ec108db2f951dd5c442b580a23
Akash Kumar <akaysharma894@gmail.com>
2021-06-24 23:50:14 +0530

    updated and Added Table2 path

--- src/components/ScreenTwo/index.js
+++ src/components/ScreenTwo/index.js
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Styles from "./ScreenTwo.module.css";
-import { ReactComponent as BackLogo } from "../../assets/icons/arrow_back.svg";
-import { ReactComponent as PencilLogo } from "../../assets/icons/pencil.svg";
 import { ReactComponent as SyncLogo } from "../../assets/icons/sync.svg";
 import { ReactComponent as PermissionLogo } from "../../assets/icons/permissions.svg";
 import { ReactComponent as ApprovalLogo } from "../../assets/icons/approval.svg";
-import { ReactComponent as AddCircleLogo } from "../../assets/icons/add_circle_outline.svg";
-import { ReactComponent as ToggleLogo } from "../../assets/icons/on-label.svg";
+import { Table2 } from "../Tables";
 
 const tableData = [
   {
     id: 1,
     department: "Budget",
     access: "All Access",
-    summary: "veiw",
+    summary: "view",
     lastUpdated: "1 min Ago",
   },
   {
     id: 2,
     department: "Bidding",
     access: "All Access",
-    summary: "veiw",
+    summary: "view",
     lastUpdated: "1 min Ago",
   },
   {
@@ -54,11 +51,6 @@ const tableData = [
 ];
 
 const ScreenTwo = () => {
-  const [expand, setExpand] = useState(false);
-  const expandHandler = () => {
-    console.log("hello");
-    setExpand(true);
-  };
   return (
     <div className={Styles["screen-wrapper"]}>
       <div className={Styles["nav-wrapper"]}>
@@ -79,49 +71,7 @@ const ScreenTwo = () => {
           <span className={Styles["text"]}>Last synced 15 mins ago</span>
         </div>
       </div>
-      <div className={Styles["table-container"]}>
-        <div className={Styles["back-nav-wrapper"]}>
-          <BackLogo />
-          <span className={Styles["nav-text"]}>Management Team</span>
-          <PencilLogo />
-        </div>
-        <div className={Styles["table-wrapper"]}>
-          <table className={Styles["table"]}>
-            <tr className={Styles["table-head"]}>
-              <th />
-              <th>Deparment/Role Name</th>
-              <th>Access Level</th>
-              <th>Summary</th>
-              <th>Last Updated</th>
-              <th />
-            </tr>
-            {tableData.map((data) => (
-              <tr key={data.id} className={Styles["data-row"]}>
-                <td className={Styles["add-circle-logo"]}>
-                  <AddCircleLogo onClick={expandHandler} />
-                </td>
-                <td>{data.department}</td>
-                <td>
-                  <div
-                    className={`${
-                      data.access === "All Access"
-                        ? Styles["access"]
-                        : Styles["restricted"]
-                    }`}
-                  >
-                    {data.access}
-                  </div>
-                </td>
-                <td>{data.summary}</td>
-                <td>{data.lastUpdated}</td>
-                <td className={Styles["eye-logo"]}>
-                  <ToggleLogo />
-                </td>
-              </tr>
-            ))}
-          </table>
-        </div>
-      </div>
+      <Table2 tableData={tableData} />
     </div>
   );
 };
